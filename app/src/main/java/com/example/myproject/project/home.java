4028a3a62299ba533f790ce39a07824cd8363bd8
khushbu rathi <55338765+khushbu2709@users.noreply.github.com>
2019-09-15 10:07:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myproject/project/home.java
@@ -0,0 +1,16 @@
+package com.example.myproject.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sourcey.materiallogindemo.R;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/myproject/signup.java
@@ -0,0 +1,16 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sourcey.materiallogindemo.R;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
